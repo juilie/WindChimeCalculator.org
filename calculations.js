70128d9bbf0984748d25989ddecb13eeda5b8c0f
Julia Humphrey Bodian <julie@Julias-MacBook-Pro-2.local>
2023-08-22 02:05:49 -0400

    move images to assets folder

--- calculations.js
+++ calculations.js
@@ -18,28 +18,79 @@ function getLengths() {
                         <th>Length</th>
                     </tr>`;
     const proportions = {
-        "1": {proportion: 1, interval:"Unison"},
-       "b2": {proportion: .9798, interval:"Minor 2nd"},
-        "2": {proportion: .9428, interval:"Major 2nd"},
-        "#2": {proportion: .9129, interval:"Augmented 2nd"},
-        "b3":{proportion: .9129, interval:"Minor 3rd"} ,
-        "3": {proportion: .8944, interval:"Major 3rd"},
-        "4":{proportion: .8660, interval:"Perfect 4th"} ,
-        "#4": {proportion: .8433, interval:"Augmented 4th"},
-        "b5": {proportion: .8433, interval:"Dimished 5th"},
-        "5": {proportion: .8165, interval:"Perfect 5th"},
-        "#5": {proportion: .7906, interval:"Augmented 5th"},
-        "b6":{proportion: .7906, interval:"Minor 6th"} ,
-        "6": {proportion: .7746, interval:"Major 6th"},
-        "º7":{proportion: .7746, interval:"Diminished 7th"} ,
-        "b7":{proportion: .7454, interval:"Minor 7th"} ,
-        "7":{proportion: .7303, interval:"Major 7th"} ,
-        "8":{proportion: .7071, interval:"Octave"} 
+        "1": {
+            proportion: 1,
+            interval: "Unison"
+        },
+        "b2": {
+            proportion: .9798,
+            interval: "Minor 2nd"
+        },
+        "2": {
+            proportion: .9428,
+            interval: "Major 2nd"
+        },
+        "#2": {
+            proportion: .9129,
+            interval: "Augmented 2nd"
+        },
+        "b3": {
+            proportion: .9129,
+            interval: "Minor 3rd"
+        },
+        "3": {
+            proportion: .8944,
+            interval: "Major 3rd"
+        },
+        "4": {
+            proportion: .8660,
+            interval: "Perfect 4th"
+        },
+        "#4": {
+            proportion: .8433,
+            interval: "Augmented 4th"
+        },
+        "b5": {
+            proportion: .8433,
+            interval: "Dimished 5th"
+        },
+        "5": {
+            proportion: .8165,
+            interval: "Perfect 5th"
+        },
+        "#5": {
+            proportion: .7906,
+            interval: "Augmented 5th"
+        },
+        "b6": {
+            proportion: .7906,
+            interval: "Minor 6th"
+        },
+        "6": {
+            proportion: .7746,
+            interval: "Major 6th"
+        },
+        "º7": {
+            proportion: .7746,
+            interval: "Diminished 7th"
+        },
+        "b7": {
+            proportion: .7454,
+            interval: "Minor 7th"
+        },
+        "7": {
+            proportion: .7303,
+            interval: "Major 7th"
+        },
+        "8": {
+            proportion: .7071,
+            interval: "Octave"
+        }
     }
 
     for (let i = 0; i < tuning.length; i++) {
         text +=
-        `<tr><td>${proportions[tuning[i]].interval}</td><td>${String((length * proportions[tuning[i]].proportion).toFixed(4))}</td></tr>`;
+            `<tr><td>${proportions[tuning[i]].interval}</td><td>${String((length * proportions[tuning[i]].proportion).toFixed(4))}</td></tr>`;
     }
     text += "</table>";
     document.getElementById("demo").innerHTML = text;
@@ -47,8 +98,7 @@ function getLengths() {
 
 var lengthInput = document.getElementById("length");
 lengthInput.addEventListener("keydown", function (e) {
-
-    if (e.code === "Enter") { //checks whether the pressed key is "Enter"
+    if (e.code === "Enter") {
         e.preventDefault();
         getLengths();
     }
